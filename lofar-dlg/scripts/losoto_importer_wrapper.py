f5857a04280252463947b0348ea9b6e2a9f720c5
Yan Grange <grange@astron.nl>
2018-02-08 17:04:00 +0100

    Dirty hack, but it works

--- lofar-dlg/scripts/losoto_importer_wrapper.py
+++ lofar-dlg/scripts/losoto_importer_wrapper.py
@@ -2,6 +2,10 @@
 
 import argparse
 import subprocess as SP
+from tempfile import mkdtemp
+from shutil import copytree, rmtree
+from os.path import join as ojoin
+from os.path import basename
 
 def check_checkmark(fil):
     print("CHECK {fil}".format(fil=fil))
@@ -15,10 +19,16 @@ parser.add_argument('-c', '--complevel', type=int, default=5,
 parser.add_argument('-o', '--output', help='Output h5 file name')
 args = parser.parse_args()
 
-cmd = ["/home/lofar-dlg/opt/prefactor/scripts/losotoImporter.py", args.output, "-c", str(args.complevel), "-s", args.solset]
-cmd.extend(args.inpfiles)
+tmp_dir = mkdtemp()
 
+for fnam in args.inpfiles:
+    copytree(fnam, ojoin(tmp_dir, basename(fnam)))
+
+cmd = ["/home/lofar-dlg/opt/prefactor/scripts/losotoImporter.py", "-c", str(args.complevel), "-s", args.solset, args.output, 
+       ojoin(tmp_dir, "*")]
 retval = SP.call(cmd)
 
 if retval != 0:
-     raise SP.CalledProcessError(retval, command)
+     raise SP.CalledProcessError(retval, cmd)
+
+rmtree(tmp_dir)
